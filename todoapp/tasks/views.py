23da05969b4e70eab49a147ab0bd7e5ba0f19879
fonsecaRafael <rafael.abreu.fonseca@gmail.com>
2023-05-08 11:52:49 +0200

    Insert pending tasks list in home view. close #2

--- todoapp/tasks/views.py
+++ todoapp/tasks/views.py
@@ -3,15 +3,18 @@ from django.shortcuts import render
 from django.urls import reverse
 
 from todoapp.tasks.forms import NewTaskForm
+from todoapp.tasks.models import Tasks
 
 
 def home(request):
+    pending_tasks = Tasks.objects.filter(done=False).all()
     if request.method == 'POST':
         form = NewTaskForm(request.POST)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect(reverse('tasks:home'))
         else:
-            return render(request, 'tasks/home.html', {'form': form}, status=400)
-
-    return render(request, 'tasks/home.html')
+            return render(request, 'tasks/home.html',
+                          {'form': form, 'pending_tasks': pending_tasks},
+                          status=400)
+    return render(request, 'tasks/home.html', {'pending_tasks': pending_tasks})
