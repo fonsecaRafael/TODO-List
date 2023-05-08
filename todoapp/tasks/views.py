43578e72420bdcd61b72a0021378e22e3af89f66
fonsecaRafael <rafael.abreu.fonseca@gmail.com>
2023-05-08 11:20:21 +0200

    Refactor view and template Tasks Home

--- todoapp/tasks/views.py
+++ todoapp/tasks/views.py
@@ -1,5 +1,17 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from todoapp.tasks.forms import NewTaskForm
 
 
 def home(request):
+    if request.method == 'POST':
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('tasks:home'))
+        else:
+            return render(request, 'tasks/home.html', {'form': form}, status=400)
+
     return render(request, 'tasks/home.html')
