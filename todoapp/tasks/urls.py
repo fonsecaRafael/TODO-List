54fca3b9f471ec06d22b70c06dbab14f9a4ae965
fonsecaRafael <rafael.abreu.fonseca@gmail.com>
2023-05-09 07:25:27 +0200

    Turn delete a valid name to named url

--- todoapp/tasks/urls.py
+++ todoapp/tasks/urls.py
@@ -7,4 +7,5 @@ app_name = 'tasks'
 urlpatterns = [
     path('', views.home, name='home'),
     path('<int:task_id>', views.detail, name='detail'),
+    path('del/<int:task_id>', views.delete, name='delete'),
 ]
